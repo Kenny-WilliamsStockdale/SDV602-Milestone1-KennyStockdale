2ba237e08823c952d958e6df42f1630cf345eaeb
Kenny <kenny-williamsStockd@live.nmit.ac.nz>
2021-08-23 17:26:44 +1200

    added other page made sure it closes cleanly

--- test-scripts.py
+++ test-scripts.py
@@ -12,18 +12,27 @@ def login_main():
         [sg.Button('Login')],
         [sg.Button('Exit Application')]]
     return sg.Window('Property Tiles Login Page', layout, finalize=True)
-    
-    
-window = login_main()
-while True:             # The Event Loop
+
+def Data_source_page():
+    layout = [
+        [sg.Button('Login')],
+        [sg.Button('Exit Application')]]
+    return sg.Window('Data Source Page', layout, finalize=True, size=(500,150))
+
+def event():
+    window1 = None  #window1 = login_main()
+    window2 = Data_source_page()
+    while True:             # The Event Loop
         window, event, values = sg.read_all_windows()
         if event == sg.WIN_CLOSED or event == 'Exit Application':
+            window.close()   # if closing win 1, exit program
             break
-window.close()
 
    
 
 if __name__ == "__main__":
     # def function here
-    login_main()
+    #login_main()
+    #Data_source_page()
+    event()
     pass
