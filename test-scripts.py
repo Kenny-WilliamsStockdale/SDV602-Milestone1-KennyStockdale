63916283bcc77e882c84d51b143b760c6eb6520f
Kenny <kenny-williamsStockd@live.nmit.ac.nz>
2021-08-24 15:33:42 +1200

    added DES1 page

--- test-scripts.py
+++ test-scripts.py
@@ -15,13 +15,24 @@ def login_main():
 
 def Data_source_page():
     layout = [
-        [sg.Button('Login')],
-        [sg.Button('Exit Application')]]
-    return sg.Window('Data Source Page', layout, finalize=True, size=(500,150))
+        [sg.Button('Issue dates'), sg.Button('Current status')],
+        [sg.Button('Number of owners'), sg.Button('Upload new data')],
+        [sg.Button('Logout')]]
+    return sg.Window('Data Source Page', layout, finalize=True, size=(500,150), element_justification='c' )
+
+def DES1():
+    layout = [
+        [sg.Button('ZOOM +'), sg.Button('ZOOM -')],
+        [sg.Multiline(default_text='Data Information Summary:', size=(35, 5)), sg.Multiline(default_text='Chat System:',size=(35, 5))],
+        [sg.Button('Next'), sg.Button('Previous')],
+        [sg.Button('Back'), sg.Button('Logout')],
+        ]
+    return sg.Window('Property Issue Dates', layout, finalize=True, size=(800,450))
 
 def event():
     window1 = None  #window1 = login_main()
     window2 = Data_source_page()
+    window3 = DES1()
     while True:             # The Event Loop
         window, event, values = sg.read_all_windows()
         if event == sg.WIN_CLOSED or event == 'Exit Application':
