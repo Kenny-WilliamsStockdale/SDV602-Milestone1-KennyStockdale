b9289e35e57f78ffbd0d6ff74dff7673fa3a28f5
Kenny <kenny-williamsStockd@live.nmit.ac.nz>
2021-08-25 15:08:46 +1200

    connected eastablished pages

--- test-scripts.py
+++ test-scripts.py
@@ -5,9 +5,11 @@ import PySimpleGUI as sg
 import matplotlib
 matplotlib.use('TkAgg')
 """
-Making test scripts for Data Explorer screens and various connecting interfaces 
- 
+Making test scripts for Data Explorer screens and various connecting interfaces
+
 """
+
+
 def login_main():
     layout = [
         [sg.Text('Username:')],
@@ -16,17 +18,48 @@ def login_main():
         [sg.InputText('')],
         [sg.Button('Login')],
         [sg.Button('Exit Application')]]
-    return sg.Window('Property Tiles Login Page', layout, finalize=True)
+    window = sg.Window('Property Tiles Login Page', layout, finalize=True)
+    event, values = window.read()
+    print(event, values)
+
+    if event == None or event == 'Exit Application':
+        window.close()
+    if event == 'Login':
+        window.close()
+        Data_source_page()
+    if event == 'Exit Application':
+        window.close()
 
 def Data_source_page():
     layout = [
-        [sg.Button('Property issue dates'), sg.Button('Current property status')],
+        [sg.Button('Property issue dates'),
+         sg.Button('Current property status')],
         [sg.Button('Number of property owners'), sg.Button('Upload new data')],
         [sg.Button('Logout')]]
-    return sg.Window('Data Source Page', layout, finalize=True, size=(500,150), element_justification='c' )
+    window = sg.Window('Data Source Page', layout, finalize=True,
+                       size=(500, 150), element_justification='c')
+
+    event, values = window.read()
+    print(event, values)
+
+    if event == None or event == 'Exit Application':
+        window.close()
+    if event == 'Property issue dates':
+        window.close()
+        DataExplorerScreen1()
+    if event == 'Current property status':
+        window.close()
+        DataExplorerScreen2()
+    if event == 'Number of property owners':
+        window.close()
+        DataExplorerScreen3()
+    if event == 'Logout':
+        window.close()
+        login_main()
+
 
 def DataExplorerScreen1():
-# ------------------------------- PASTE YOUR MATPLOTLIB CODE HERE -------------------------------
+    # ------------------------------- PASTE YOUR MATPLOTLIB CODE HERE -------------------------------
     labels = ['G1', 'G2', 'G3', 'G4', 'G5']
     men_means = [20, 34, 30, 35, 27]
     women_means = [25, 32, 34, 20, 25]
@@ -54,29 +87,31 @@ def DataExplorerScreen1():
     # ------------------------------- Beginning of Matplotlib helper code -----------------------
 
     def draw_figure(canvas, figure):
-            figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
-            figure_canvas_agg.draw()
-            figure_canvas_agg.get_tk_widget().pack(side='top', fill='both', expand=1)
-            return figure_canvas_agg
+        figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
+        figure_canvas_agg.draw()
+        figure_canvas_agg.get_tk_widget().pack(side='top', fill='both', expand=1)
+        return figure_canvas_agg
 
     # ------------------------------- Beginning of GUI CODE -------------------------------
 
     # define the window layout
     layout = [[sg.Canvas(key='-CANVAS-')],
-            [sg.Button('ZOOM +'), sg.Button('ZOOM -')],
-            [sg.Multiline(default_text='Data Information Summary:', size=(35, 5)), sg.Multiline(default_text='Chat System:',size=(35, 5))],
-            [sg.Button('Previous'), sg.Button('Next')],
-            [sg.Button('Back'), sg.Button('Logout')]]
+              [sg.Button('ZOOM +'), sg.Button('ZOOM -')],
+              [sg.Multiline(default_text='Data Information Summary:', size=(
+                  35, 5)), sg.Multiline(default_text='Chat System:', size=(35, 5))],
+              [sg.Button('Previous'), sg.Button('Next')],
+              [sg.Button('Back'), sg.Button('Logout')]]
 
     # create the form and show it without the plot
-    window = sg.Window('Property issue dates', layout, finalize=True, element_justification='center', size=(800, 700))
+    window = sg.Window('Property issue dates', layout, finalize=True,
+                       element_justification='center', size=(800, 700))
 
     # add the plot to the window
     fig_canvas_agg = draw_figure(window['-CANVAS-'].TKCanvas, fig)
 
     event, values = window.read()
     print(event, values)
-    
+
     if event == None or event == 'Exit Application':
         window.close()
     if event == 'Previous':
@@ -85,9 +120,16 @@ def DataExplorerScreen1():
     if event == 'Next':
         window.close()
         DataExplorerScreen2()
-            
+    if event == 'Back':
+        window.close()
+        Data_source_page()
+    if event == 'Logout':
+        window.close()
+        login_main()
+
+
 def DataExplorerScreen2():
-# ------------------------------- PASTE YOUR MATPLOTLIB CODE HERE -------------------------------
+    # ------------------------------- PASTE YOUR MATPLOTLIB CODE HERE -------------------------------
     labels = ['G1', 'G2', 'G3', 'G4', 'G5']
     men_means = [20, 34, 30, 35, 27]
     women_means = [25, 32, 34, 20, 25]
@@ -115,29 +157,31 @@ def DataExplorerScreen2():
     # ------------------------------- Beginning of Matplotlib helper code -----------------------
 
     def draw_figure(canvas, figure):
-            figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
-            figure_canvas_agg.draw()
-            figure_canvas_agg.get_tk_widget().pack(side='top', fill='both', expand=1)
-            return figure_canvas_agg
+        figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
+        figure_canvas_agg.draw()
+        figure_canvas_agg.get_tk_widget().pack(side='top', fill='both', expand=1)
+        return figure_canvas_agg
 
     # ------------------------------- Beginning of GUI CODE -------------------------------
 
     # define the window layout
     layout = [[sg.Canvas(key='-CANVAS-')],
-            [sg.Button('ZOOM +'), sg.Button('ZOOM -')],
-            [sg.Multiline(default_text='Data Information Summary:', size=(35, 5)), sg.Multiline(default_text='Chat System:',size=(35, 5))],
-            [sg.Button('Previous'), sg.Button('Next')],
-            [sg.Button('Back'), sg.Button('Logout')]]
+              [sg.Button('ZOOM +'), sg.Button('ZOOM -')],
+              [sg.Multiline(default_text='Data Information Summary:', size=(
+                  35, 5)), sg.Multiline(default_text='Chat System:', size=(35, 5))],
+              [sg.Button('Previous'), sg.Button('Next')],
+              [sg.Button('Back'), sg.Button('Logout')]]
 
     # create the form and show it without the plot
-    window = sg.Window('Current property status', layout, finalize=True, element_justification='center', size=(800, 700))
+    window = sg.Window('Current property status', layout, finalize=True,
+                       element_justification='center', size=(800, 700))
 
     # add the plot to the window
     fig_canvas_agg = draw_figure(window['-CANVAS-'].TKCanvas, fig)
 
     event, values = window.read()
     print(event, values)
-    
+
     if event == None or event == 'Exit Application':
         window.close()
     if event == 'Previous':
@@ -146,9 +190,16 @@ def DataExplorerScreen2():
     if event == 'Next':
         window.close()
         DataExplorerScreen3()
-    
+    if event == 'Back':
+        window.close()
+        Data_source_page()
+    if event == 'Logout':
+        window.close()
+        login_main()
+
+
 def DataExplorerScreen3():
-# ------------------------------- PASTE YOUR MATPLOTLIB CODE HERE -------------------------------
+    # ------------------------------- PASTE YOUR MATPLOTLIB CODE HERE -------------------------------
     labels = ['G1', 'G2', 'G3', 'G4', 'G5']
     men_means = [20, 34, 30, 35, 27]
     women_means = [25, 32, 34, 20, 25]
@@ -176,29 +227,31 @@ def DataExplorerScreen3():
     # ------------------------------- Beginning of Matplotlib helper code -----------------------
 
     def draw_figure(canvas, figure):
-            figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
-            figure_canvas_agg.draw()
-            figure_canvas_agg.get_tk_widget().pack(side='top', fill='both', expand=1)
-            return figure_canvas_agg
+        figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
+        figure_canvas_agg.draw()
+        figure_canvas_agg.get_tk_widget().pack(side='top', fill='both', expand=1)
+        return figure_canvas_agg
 
     # ------------------------------- Beginning of GUI CODE -------------------------------
 
     # define the window layout
     layout = [[sg.Canvas(key='-CANVAS-')],
-            [sg.Button('ZOOM +'), sg.Button('ZOOM -')],
-            [sg.Multiline(default_text='Data Information Summary:', size=(35, 5)), sg.Multiline(default_text='Chat System:',size=(35, 5))],
-            [sg.Button('Previous'), sg.Button('Next')],
-            [sg.Button('Back'), sg.Button('Logout')]]
+              [sg.Button('ZOOM +'), sg.Button('ZOOM -')],
+              [sg.Multiline(default_text='Data Information Summary:', size=(
+                  35, 5)), sg.Multiline(default_text='Chat System:', size=(35, 5))],
+              [sg.Button('Previous'), sg.Button('Next')],
+              [sg.Button('Back'), sg.Button('Logout')]]
 
     # create the form and show it without the plot
-    window = sg.Window('Number of property owners', layout, finalize=True, element_justification='center', size=(800, 700))
+    window = sg.Window('Number of property owners', layout,
+                       finalize=True, element_justification='center', size=(800, 700))
 
     # add the plot to the window
     fig_canvas_agg = draw_figure(window['-CANVAS-'].TKCanvas, fig)
 
     event, values = window.read()
     print(event, values)
-    
+
     if event == None or event == 'Exit Application':
         window.close()
     if event == 'Previous':
@@ -207,7 +260,12 @@ def DataExplorerScreen3():
     if event == 'Next':
         window.close()
         DataExplorerScreen1()
-        
+    if event == 'Back':
+        window.close()
+        Data_source_page()
+    if event == 'Logout':
+        window.close()
+        login_main()
 
 # def event():
 #     window = DataExplorerScreen1()  #window1 = login_main()
@@ -215,14 +273,13 @@ def DataExplorerScreen3():
 #         event, values = window.read()
 #         if event == None or event == 'Exit Application':
 #             window.close()
-#             break  
+#             break
 
-   
 
 if __name__ == "__main__":
     # def function here
-    #login_main()
-    #Data_source_page()
+    login_main()
+    # Data_source_page()
     # event()
-    DataExplorerScreen1()
+    # DataExplorerScreen1()
     pass
